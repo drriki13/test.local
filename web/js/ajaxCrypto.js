ac791009f01d0f3a979a486121c3864eeb1659c3
riki13 <dr.riki13@yandex.ru>
2021-02-17 18:06:00 +0300

    17.02.2021-v2

--- web/js/ajaxCrypto.js
+++ web/js/ajaxCrypto.js
@@ -1,33 +1,26 @@
-// $( document ).ready(function() {
-//
-//
-//
-//     $('#cryptoform-altcoin').on('change', function(e) {
-//         e.preventDefault();
-//         let selectorAjax = $(this);
-//         console.log(selectorAjax.serializeArray());
-//         sendAjax(selectorAjax.serializeArray());
-//     });
-//
-//     $('#cryptoform-currencies').on('change', function(e) {
-//         e.preventDefault();
-//         let checkBoxAjax = $(this);
-//         console.log(checkBoxAjax);
-//
-//     });
-// });
-//
-// function sendAjax(dataForm) {
-//
-//     let url = '/crypto/index';
-//     let data = {data: dataForm};
-//
-//     $.post(url, data).done(function(data) {
-//         console.log(data);
-//         if (data.success){
-//             //let html = data.view;
-//         }else{
-//             console.log(data.error)
-//         }
-//     });
-// }
+let form = $('#w0');
+let data = [];
+
+$(document).ready(function() {
+    $('#cryptoform-altcoin').on('change', function(e) {
+        e.preventDefault();
+        data = form.serializeArray();
+        sendData(data);
+    });
+
+    $('#cryptoform-currencies input').on('change', function(e) {
+        e.preventDefault();
+        data = form.serializeArray();
+        sendData(data);
+    });
+});
+
+
+function sendData(data) {
+    let url = '/crypto/send-request';
+    $.get(url, data).done(function(data) {
+        if (data.success) {
+            $('#js-ajax-wrap-html').html(data.html);
+        }
+    });
+}
